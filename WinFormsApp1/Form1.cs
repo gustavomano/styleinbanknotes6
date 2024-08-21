ca87e5ac80ac95e703dce18891988ebd40f4245a
cj3022129 <cj3022129@LAB03-PC13>
2024-08-21 12:55:53 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,26 @@
+using System.Security.Cryptography;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Telalogin frm = new Telalogin();
+            this.Visible = false;
+            frm.ShowDialog();
+            this.Visible = true;
+
+        }
+    }
+}
