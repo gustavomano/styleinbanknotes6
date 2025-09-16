26c8da04d143246ad39e065d12a702ba01977e57
gustavo <160731397+gustavomano@users.noreply.github.com>
2025-09-16 08:10:16 -0300

    Update HashHelper.cs

--- WinFormsApp1/Helpers/HashHelper.cs
+++ WinFormsApp1/Helpers/HashHelper.cs
@@ -1,1 +1,13 @@
+using BCrypt.Net;
 
+namespace WinFormsApp1.Helpers
+{
+    public static class HashHelper
+    {
+        public static string Hash(string senha) =>
+            BCrypt.Net.BCrypt.HashPassword(senha);
+
+        public static bool Verificar(string senha, string hash) =>
+            BCrypt.Net.BCrypt.Verify(senha, hash);
+    }
+}
